30056e46ae6ba7253fe3efbcb18cd17be27244d8
Testing <kathy.htun@spiceworks.co.jp>
2018-12-09 23:10:11 +0630

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+// $('.slider').slick({
+//   // centerPadding: '60px',
+//   slidesToShow: 3,
+//   dots: true,
+//   responsive: [
+//     {
+//       breakpoint: 990,
+//       settings: {
+//         centerPadding: '68px',
+//         slidesToShow: 1,
+//         centerMode: true
+//       }
+//     }
+//   ]
+// });
